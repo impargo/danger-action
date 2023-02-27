811c34cc561b731ed1f0678435541daf24e2ce4c
david-paulus-saad <d.paulus@impargo.de>
2023-02-28 01:34:02 +0200

    feat: adapt the rules

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,16 +1,14 @@
-import { danger, fail, warn } from 'danger'
+import { danger, warn, message } from 'danger'
+import mentor from 'danger-plugin-mentor'
+
+
+mentor()
 
 // No PR is too small to include a description of why you made a change
 if (danger.github.pr.body.length < 100) {
   warn('Please include a detailed description of your PR changes, at least 100 characters.')
 }
 
-// Check that someone has been assigned to this PR
-if (danger.github.pr.assignee === null) {
-  warn(
-    'Please assign someone to Review this PR.',
-  )
-}
 
 // Request changes to src also include changes to tests.
 const allFiles = danger.git.modified_files.concat(danger.git.created_files)
@@ -34,15 +32,36 @@ if (!hasDescTitle) {
 
 const migrationPattern = /migration/im
 
-const hasMigrationChanges = allFiles.some((p) => p.includes('database-migration/'))
+const hasMigrationChanges = allFiles.some((p) => p.includes('migrations/'))
 
 if (migrationPattern.test(danger.github.pr.title) || hasMigrationChanges) {
   warn('Please Pay attention to the Migrations on dev')
   warn('Add a manual testing for the Migration in the description!')
+  message(`
+  # Test MongoDB Database Migration Locally
+  - get the string connection of the database
+  - dump the collections that you want to test by \`mongodump --uri "DB_CONNECTION_STRING" --collection COLLECTION_NAME  -vvvv --forceTableScan --out -  >> COLLECTION_NAME.bson\`
+   > you need to do that for each collection
+  - restore the collections to a local MongoDB \`mongorestore --port <port> --db <destination database> --collection <collection-name> <data-dump-path/dbname/collection.bson> --drop\`
+  - change the local configs and secrets to use the local MongoDB.
+  - then run the Job using \`ENVIRONMENT=local skaffold run\`
+  - test it by starting the services and the front-end locally and make sure they use the local database.
+  - if the migration works properly, then we can merge the PR to the dev, and the database migration job will apply the changes.
+  `)
 }
 
-if (danger.git.commits.length > 5) {
-  fail(
-    'Keep PRs short, and have less commits!',
+const bigPRThreshold = 20
+const linesCount = danger.git.linesOfCode("**/*"); // add await 
+const excludeLinesCount = danger.git.linesOfCode("(.*(\.snap|\.test.*|mock.*|json|xml))"); // add await
+const totalLinesCount = linesCount - excludeLinesCount;
+
+if (danger.git.commits.length > 20 || totalLinesCount > bigPRThreshold) {
+  warn(
+    'Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.!',
   )
 }
+
+if (danger.github.pr.deletions > danger.github.pr.additions) {
+  message(`:thumbsup: You removed more code than added!`);
+}
+
